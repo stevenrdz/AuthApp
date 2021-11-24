4540d68018e252dbc120d78b13e585ed8345bb0e
stevenrdz <stevenrodriguezm97@gmail.com>
2021-11-24 16:57:03 -0500

    24/11 07-Front

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent{
+
+  miFormulario: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+  login(){}
+}
